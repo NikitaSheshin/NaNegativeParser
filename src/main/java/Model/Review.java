98beacc9e5d5441f5f310ffe0c9da6601563d0ab
Nikita Shehsin <sheshin.nikita21@yandex.ru>
2022-11-10 18:27:18 +0300

    Добавил модель для хранения обзора

---
+++ src/main/java/Model/Review.java
@@ -0,0 +1,14 @@
+package Model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+public class Review {
+    String pluses;
+    String minuses;
+    String reviewText;
+}
